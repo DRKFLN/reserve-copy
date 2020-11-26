5660041cd8e671cb45bc4dc4805212996078aa93
DRKFLN <arata.oni16@gmail.com>
2020-11-26 13:31:03 +0300

    v2

---
+++ reserve copy/reserve copy/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Diagnostics;
+namespace reserve_copy
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
